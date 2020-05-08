86fa9470809702380afcd8a173ea878a8062127a
Andrea Tchalaou <attchalaou@gmail.com>
2020-05-08 17:56:32 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,19 +1,5 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-
-end
-
 def greeting
   puts "Hello World"
 end
